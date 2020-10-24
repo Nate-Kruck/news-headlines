2f14e67403b5e1228f06d96c38d87c669e49d97a
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-10-23 19:09:51 -0700

    Revert "Revert "map function not working""

---
+++ src/components/articles/ArticleList.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Articles.css';
+import Article from '../../Components/Article/Article';
+
+function ArticleList({ articles }) {
+  const ArticleElements = articles.map(article => (
+    <Article key={article.title} article={article}/>
+  ));
+
+  return (
+    <div className={styles.articles}>
+      <ul>{ArticleElements}</ul>
+    </div>
+  );
+}
+
+ArticleList.propTypes = {
+  articles: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    author: PropTypes.string,
+    description: PropTypes.string,
+    imageUrl: PropTypes.string,
+    url: PropTypes.string
+  })).isRequired
+};
+
+export default ArticleList;
